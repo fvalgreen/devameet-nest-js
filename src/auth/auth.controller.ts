8ca6f07c075dcb892189f92c3086caab4a0a5bd6
Felipe Valverde <62726786+fvalgreen@users.noreply.github.com>
2023-04-08 13:20:14 -0300

    Cadastro

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, HttpCode, HttpStatus, Body } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { LoginDto } from "./dtos/login.dto";
+import { RegisterDto } from "src/user/dtos/register.dto";
 
 @Controller('auth')
 
@@ -11,5 +12,11 @@ export class AuthController{
       @HttpCode(HttpStatus.OK)
       login(@Body() dto: LoginDto){
         return this.authService.login(dto);
+      }
+
+      @Post('register')
+      @HttpCode(HttpStatus.OK)
+      register(@Body() dto: RegisterDto){
+        return this.authService.register(dto);
       }
 }
