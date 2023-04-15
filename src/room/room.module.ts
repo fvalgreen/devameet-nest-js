fb83d26b54e5568bc878373967317d9d9985424c
Felipe Valverde <62726786+fvalgreen@users.noreply.github.com>
2023-04-15 19:49:03 -0300

    Web Socket

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -5,6 +5,7 @@ import { MeetModule } from 'src/meet/meet.module';
 import { UserModule } from 'src/user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Position, PositionSchema } from './schemas/position.schema';
+import { RoomGateway } from './room.gateway';
 
 @Module({
   imports: [
@@ -15,7 +16,7 @@ import { Position, PositionSchema } from './schemas/position.schema';
       schema: PositionSchema}
     ])
   ],
-  providers: [RoomService],
+  providers: [RoomService, RoomGateway],
   controllers: [RoomController]
 })
 export class RoomModule {}
