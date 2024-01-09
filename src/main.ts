89faa365a0174a7ae6659948066df5bb47e073dc
Felipe Valverde <62726786+fvalgreen@users.noreply.github.com>
2024-01-09 19:41:51 -0300

    Teste

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,7 @@ async function bootstrap() {
 
   app.enableCors({
     "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE"
   });
 
   app.useGlobalPipes(
