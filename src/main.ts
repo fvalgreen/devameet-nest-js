cbe3f6c15079f6ea5113430016cc0923b7798223
Felipe Valverde <62726786+fvalgreen@users.noreply.github.com>
2024-04-02 21:04:37 -0300

    Teste

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,13 @@ async function bootstrap() {
     logger: ['debug', 'error', 'log', 'warn']
   });
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
   app.enableCors({
     "origin": ["*"],
     "methods": ["GET","HEAD","PUT","POST","DELETE", "OPTIONS"]
